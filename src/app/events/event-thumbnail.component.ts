47adeb69a3ee8e5d04116b6a0a1e01f02b323390
Mindaugas <mliktas@gmail.com>
2018-12-16 23:46:27 +0200

    Some ng template syntax

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -4,18 +4,31 @@ import { Component, Input } from '@angular/core';
     selector: 'event-thumbnail',
     template: `
         <div class="well hoverwell thumbnail">
-            <h2>{{event.name}}</h2>
-            <div>Date: {{event.date}}</div>
-            <div>Time: {{event.time}}</div>
-            <div>Price \${{event.price}}</div>
-            <div>
-                <span>Location: {{event.location.address}}</span>
-                <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+            <h2>{{event?.name}}</h2>
+            <div>Date: {{event?.date}}</div>
+            <div [ngStyle]="getStartTimeStyle()"
+                [ngSwitch]="event?.time">
+                Time: {{event?.time}}
+                <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+                <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+                <span *ngSwitchDefault>(Normal Start)</span>
             </div>
+            <div>Price \${{event?.price}}</div>
+            <div *ngIf="event?.location">
+                <span>Location: {{event?.location?.address}}</span>
+                <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
+            </div>
+            <div *ngIf="event?.onlineUrl">Online URL: {{event?.onlineUrl}}</div>
         </div>
     `,
     styles: [
         `
+        .bold{
+            font-weight: bold;
+        }
+        .green{
+            color: #003300 !important;
+        }
         .thumbnail{
             min-height: 210px;
         }
@@ -31,4 +44,12 @@ import { Component, Input } from '@angular/core';
 
 export class EventThumbnailComponent{
     @Input() event: any
+
+    getStartTimeStyle():any {
+        if(this.event && this.event.time === '8:00 am'){
+            return { color: '#003300', 'font-weight': 'bold'}
+        }
+
+        return {}
+    }
 }
