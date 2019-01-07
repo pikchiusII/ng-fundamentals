724ca6d50daabf40f58c79c00b035782cacfc606
Minde <mliktas@gmail.com>
2019-01-07 10:41:55 +0200

    Understanding Angular's dependency injections

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, Inject } from '@angular/core'
 import { FormControl, FormGroup, Validators } from '@angular/forms'
 import { AuthService } from './auth.service'
 import { Router } from '@angular/router'
+import { TOASTR_TOKEN, Toastr } from '../common/toastr.service'
 
 @Component({
   templateUrl: './profile.component.html',
@@ -34,7 +35,9 @@ export class ProfileComponent implements OnInit {
   firstName: FormControl
   lastName: FormControl
 
-  constructor(private authService: AuthService, private router: Router){
+  constructor(private authService: AuthService,
+              private router: Router,
+              @Inject(TOASTR_TOKEN) private toastr: Toastr){
 
   }
 
@@ -58,7 +61,7 @@ export class ProfileComponent implements OnInit {
   saveProfile(formValues){
     if(this.profileForm.valid){
       this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
-      this.router.navigate(['events'])
+      this.toastr.success('Profile Saved')
     }
   }
 
