39235c830ec35c6912125da29a655bf723227dfd
Mindaugas <mliktas@gmail.com>
2019-01-13 22:58:10 +0200

    Moving data storage to the server

--- src/app/events/shared/event.service.ts
+++ src/app/events/shared/event.service.ts
@@ -1,53 +1,66 @@
 import { Injectable, EventEmitter } from '@angular/core';
-import { Subject, Observable } from 'rxjs';
+import { Subject, Observable, of } from 'rxjs';
 import { IEvent, ISession } from './event.model'
+import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class EventService{
-    getEvents():Observable<IEvent[]>{
-      let subject = new Subject<IEvent[]>()
-      setTimeout(() => {subject.next(EVENTS); subject.complete(); }, 100)
-      return subject
+
+  constructor(private http: HttpClient){
+    
+  }
+
+  getEvents():Observable<IEvent[]>{
+    // .pipe is only for error handling
+    return this.http.get<IEvent[]>('/api/events').pipe(catchError(this.handleError<IEvent[]>('getEvents', [])));
+  }
+
+  private handleError<T> (operation = 'operation', result?: T){
+    return (error: any): Observable<T> => {
+      console.error(error);
+      return of(result as T);
     }
+  }
 
-    getEvent(id: number):IEvent {
-      return EVENTS.find(event => event.id === id)
-    }
+  getEvent(id: number):IEvent {
+    return EVENTS.find(event => event.id === id)
+  }
 
-    saveEvent(event){
-      event.id = 999
-      event.session = []
-      EVENTS.push(event)
-    }
+  saveEvent(event){
+    event.id = 999
+    event.session = []
+    EVENTS.push(event)
+  }
 
-    updateEvent(event){
-      let index = EVENTS.findIndex(x => x.id = event.id)
-      EVENTS[index] = event
-    }
+  updateEvent(event){
+    let index = EVENTS.findIndex(x => x.id = event.id)
+    EVENTS[index] = event
+  }
 
-    searchSessions(searchTerm: string){
-      var term = searchTerm.toLocaleLowerCase();
-      var results: ISession[] = [];
+  searchSessions(searchTerm: string){
+    var term = searchTerm.toLocaleLowerCase();
+    var results: ISession[] = [];
 
-      EVENTS.forEach(event => {
-        var matchingSessions = event.sessions.filter(session =>
-          session.name.toLocaleLowerCase().indexOf(term) > -1);
-          matchingSessions = matchingSessions.map((session: any) => {
-            session.eventId = event.id;
-            return session;
-          })
+    EVENTS.forEach(event => {
+      var matchingSessions = event.sessions.filter(session =>
+        session.name.toLocaleLowerCase().indexOf(term) > -1);
+        matchingSessions = matchingSessions.map((session: any) => {
+          session.eventId = event.id;
+          return session;
+        })
 
-          results = results.concat(matchingSessions);
-      })
+        results = results.concat(matchingSessions);
+    })
 
-      var emitter = new EventEmitter(true);
-      setTimeout(() => {
-        emitter.emit(results);
+    var emitter = new EventEmitter(true);
+    setTimeout(() => {
+      emitter.emit(results);
 
-      }, 100);
+    }, 100);
 
-      return emitter;
-    }
+    return emitter;
+  }
 }
 
 const EVENTS: IEvent[] = [
