52a2e37513e8a684800704e952b7e3bd0c291acf
Mindaugas <mliktas@gmail.com>
2020-01-13 22:11:40 +0100

    TSLint

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges } from '@angular/core'
+import { Component, Input, OnChanges } from '@angular/core';
 import { ISession } from '../shared';
 import { AuthService } from '../../user/auth.service';
 import { VoterService } from './voter.service';
@@ -15,13 +15,13 @@ export class SessionListComponent implements OnChanges {
     @Input() eventId: number;
     visibleSessions: ISession[] = [];
 
-    constructor(private auth: AuthService, private voterService: VoterService){
-        
+    constructor(private auth: AuthService, private voterService: VoterService) {
+
     }
 
-    ngOnChanges(){
+    ngOnChanges() {
         // checks if sessions are set
-        if(this.sessions){
+        if (this.sessions) {
             this.filterSessions(this.filterBy);
             this.sortBy === 'name'
                 ? this.visibleSessions.sort(sortByNameAsc)
@@ -29,42 +29,40 @@ export class SessionListComponent implements OnChanges {
         }
     }
 
-    toggleVote(session: ISession){
-        if(this.userHasVoted(session)){
+    toggleVote(session: ISession) {
+        if (this.userHasVoted(session)) {
             this.voterService.deleteVoter(this.eventId, session, this.auth.currentUser.userName);
-        }
-        else{
+        } else {
             this.voterService.addVoter(this.eventId, session, this.auth.currentUser.userName);
         }
 
-        if(this.sortBy === 'votes'){
+        if (this.sortBy === 'votes') {
             this.visibleSessions.sort(sortByVotesDesc);
         }
     }
 
-    userHasVoted(session: ISession){
+    userHasVoted(session: ISession) {
         return this.voterService.userHasVoted(session, this.auth.currentUser.userName);
     }
 
-    filterSessions(filter){
-        if(filter === 'all'){
+    filterSessions(filter) {
+        if (filter === 'all') {
             // clone of session arrays, not same reference
             this.visibleSessions = this.sessions.slice(0);
-        }
-        else {
+        } else {
             this.visibleSessions = this.sessions.filter(session => {
                 return session.level.toLocaleLowerCase() === filter;
-            })
+        });
         }
     }
 }
 
-function sortByNameAsc(s1: ISession, s2: ISession){
-    if(s1.name > s2.name) return 1
-    else if(s1.name === s2.name) return 0
-    else return -1
+function sortByNameAsc(s1: ISession, s2: ISession) {
+    if (s1.name > s2.name) { return 1; }
+    else if (s1.name === s2.name) { return 0; }
+    else { return -1; }
 }
 
-function sortByVotesDesc(s1: ISession, s2: ISession){
+function sortByVotesDesc(s1: ISession, s2: ISession) {
     return s2.voters.length - s1.voters.length;
 }
